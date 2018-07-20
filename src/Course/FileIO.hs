ca3cf0ce000ca32337231dade3711b9c1393c09f
Raymond Tay <raymondtay1974@gmail.com>
2018-07-20 08:44:00 +0800

    my rendition

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,54 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile filename contents =
+  putStrLn ("file name ----" ++ filename) >> 
+  putStrLn ("file contents ---" ++ contents)
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles fs = void . sequence $ (uncurry printFile) <$> fs
+-- so void :: IO a -> IO ()  and `sequence` when applied would give me `IO List()`
+-- from List( IO () ) and when i compose them i.e. void . sequence $ whatever
+-- would give me the desired result.
+-- Curiously, i dont think `void` actually does anything other than the fact to
+-- make the typechecker think its legal.
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile filename = do
+  contents <- readFile filename
+  return (filename, contents)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles filenames = sequence $ (<$>) getFile filenames  
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run filename = do
+  content <- readFile filename
+  results <- getFiles (lines content)
+  printFiles results
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = getArgs >>= \cliArg -> case cliArg of
+                                  f:.Nil -> run f
+                                  otherwise -> putStrLn "you are expected to pass me filenames"
 
 ----
 
